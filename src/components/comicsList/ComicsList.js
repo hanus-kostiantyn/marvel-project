36f5c2b02f4018a8fc52fe5c1f3cc964688074cf
hanus-kostiantyn <hanus.kostiantyn@gmail.com>
2024-09-11 09:05:34 +0200

    modify ComicsList and  MarvelService

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -7,17 +7,18 @@ import "./comicsList.scss";
 const ComicsList = () => {
     const [comicsList, setComicsList] = useState([]);
     const [newItemLoading, setNewItemLoading] = useState(false);
+    const [offset, setOffset] = useState(0);
     const [comicsEnded, setComicsEnded] = useState(false);
 
     const { loading, error, getAllComics } = useMarvelService();
 
     useEffect(() => {
-        onRequest(true);
+        onRequest(offset, true);
     }, []);
 
-    const onRequest = (initial) => {
+    const onRequest = (offset, initial) => {
         initial ? setNewItemLoading(false) : setNewItemLoading(true);
-        getAllComics().then(onComicsListLoaded);
+        getAllComics(offset).then(onComicsListLoaded);
     };
 
     const onComicsListLoaded = (newComicsList) => {
@@ -26,58 +27,26 @@ const ComicsList = () => {
             ended = true;
         }
 
-        setComicsList((comicsList) => [...comicsList, ...newComicsList]);
+        setComicsList([...comicsList, ...newComicsList]);
         setNewItemLoading(false);
+        setOffset(offset + 8);
         setComicsEnded(ended);
     };
 
-    console.log(comicsList);
-
     function renderItems(arr) {
         const items = arr.map((item, i) => {
-            let imgStyle = { objectFit: "cover" };
-            if (
-                item.thumbnail ===
-                "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
-            ) {
-                imgStyle = { objectFit: "unset" };
-            }
             return (
-                <li className="comics__item" key={item.id}>
+                <li className="comics__item" key={i}>
                     <a href="#">
                         <img
                             src={item.thumbnail}
                             alt={item.title}
-                            style={imgStyle}
                             className="comics__item-img"
                         />
                         <div className="comics__item-name">{item.title}</div>
                         <div className="comics__item-price">{item.price}</div>
                     </a>
                 </li>
-                // <li
-                //     key={item.id}
-                //     tabIndex={0}
-                //     ref={(el) => (itemRefs.current[i] = el)}
-                //     className="char__item"
-                //     onClick={() => {
-                //         props.onCharSelected(item.id);
-                //         focusOnItem(i);
-                //     }}
-                //     onKeyDown={(e) => {
-                //         if (e.key === " " || e.key === "Enter") {
-                //             props.onCharSelected(item.id);
-                //             focusOnItem(i);
-                //         }
-                //     }}
-                // >
-                //     <img
-                //         src={item.thumbnail}
-                //         style={imgStyle}
-                //         alt={item.name}
-                //     />
-                //     <div className="char__name">{item.name}</div>
-                // </li>
             );
         });
         return <ul className="comics__grid">{items}</ul>;
@@ -97,7 +66,7 @@ const ComicsList = () => {
                 className="button button__main button__long"
                 disabled={newItemLoading}
                 style={{ display: comicsEnded ? "none" : "block" }}
-                onClick={() => onRequest()}
+                onClick={() => onRequest(offset)}
             >
                 <div className="inner">load more</div>
             </button>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -18,9 +18,15 @@ const useMarvelService = () => {
         return _transformCharacter(res.data.results[0]);
     };
 
-    const getAllComics = async () => {
-        const res = await request(`${_apiBase}comics?limit=8&${_apiKey}`);
+    const getAllComics = async (offset = 0) => {
+        const res = await request(
+            `${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
+        );
         return res.data.results.map(_transformComics);
+    };
+    const getComics = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
     };
 
     const _transformCharacter = (char) => {
@@ -34,19 +40,23 @@ const useMarvelService = () => {
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
             comics: char.comics.items,
-            title: char.title,
         };
     };
 
-    const _transformComics = (char) => {
+    const _transformComics = (comics) => {
         return {
-            id: char.id,
-            title: char.title,
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || "There is no description",
+            pageCount: comics.pageCount
+                ? `${comics.pageCount} p.`
+                : "No information about the number of pages",
+            language: comics.textObjects[0]?.language || "en-us",
             price:
-                char.prices && char.prices[0].price
-                    ? `${char.prices[0].price}$`
+                comics.prices && comics.prices[0].price
+                    ? `${comics.prices[0].price}$`
                     : "NOT AVAILABLE",
-            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
         };
     };
 
@@ -57,6 +67,7 @@ const useMarvelService = () => {
         getAllCharacters,
         getCharacter,
         getAllComics,
+        getComics,
     };
 };
 
